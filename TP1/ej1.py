af5d8918a49a6f2084108d8a87dac459a7ee0c89
Julián Dabbah <julian@86-49-244-190.fibertel.com.ar>
2013-04-28 20:12:17 -0300

    comentarios de lo anterior

--- TP1/ej1.py
+++ TP1/ej1.py
@@ -1,13 +1,16 @@
 #Le pongo .py para tener el syntax highlight de python, en realidad hay que correrlo con scapy
 
 def quienTiene(ip):
+	#Creamos un nuevo paquete ARP y le seteamos la dirección ip cuya mac quiero averiguar
 	arp = ARP()
 	arp.setfieldval('pdst', ip)
+	#Lo mandamos y esperamos respuesta
 	rec = sr(arp, timeout = 1)
+	#Lo mostramos lindo
 	rec[0].show()
+	#Armamos una lista de mappings 'ip': dir ip, 'mac': dir mac (como para hacer algo más)
 	lst = []
 	for pkg in rec[0]:
 		lst.append({'ip': pkg[0].pdst, 'mac' : pkg[1].hwsrc })
 	return lst
-
-
+	
