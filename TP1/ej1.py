d9a3c5bdaa457395c8c92fc9507840124b03b345
ChurroCode <code@churro.com>
2013-04-28 22:10:44 -0300

    Agrega una funcion para mandar muchas consultas ARP en un rango de ips

--- TP1/ej1.py
+++ TP1/ej1.py
@@ -10,7 +10,24 @@ def quienTiene(ip):
 	rec[0].show()
 	#Armamos una lista de mappings 'ip': dir ip, 'mac': dir mac (como para hacer algo más)
 	lst = []
-	for pkg in rec[0]:
-		lst.append({'ip': pkg[0].pdst, 'mac' : pkg[1].hwsrc })
+	if len(rec) > 0:
+		for pkg in rec[0]:
+			lst.append({'ip': pkg[0].pdst, 'mac' : pkg[1].hwsrc })
 	return lst
+
+def buscar(ipBase, step = 1):
+	"""ipBase es de la forma XX.XX.XX., step es cada cuántas IPs queremos preguntar"""
+	maps = {}
+	for i in range(1, 255 / step):
+		ipArmada = ipBase + str(step * i)
+		ips = quienTiene(ipArmada)
+		if len(ips) == 0:
+			maps[ipArmada] = []
+		else:
+			for i in ips:
+				if i['ip'] in maps:
+					maps[i['ip']].append(i['mac'])
+				else:
+					maps[i['ip']] = [i['mac']]
+	return maps
 	
