fd2fd4ce2b080c69d341156c135558dc8fd28e1c
ChurroCode <code@churro.com>
2013-05-07 05:54:31 -0300

    Tidy code :)

--- TP1/src/Entropimetro.py
+++ TP1/src/Entropimetro.py
@@ -1,45 +1,9 @@
-#!/usr/bin/python
 #coding: utf-8
 
-import sys
-
-import sys
 import datetime
-import os
-import re
 import collections
 import math
 import parser
-
-
-# TODO LO COMENTADO NO SIRVE PARA ENTREGAR, PERO ME DA PENA BORRARLO.
-# params_readTime  = 'readtime' #con qué formato interpreto el timestamp
-# params_timePlace = 'timeplace' #en qué índice de split(línea de entrada) está el timestamp
-# params_history = 'history' #en minutos, cuánta historia consideramos
-# params_dataPlace = 'dataplace' #qué índices de split(línea de entrada) consideramos como información
-# params_live = 'live'
-
-# params = {params_readTime : None,
-# 	params_timePlace : None,
-# 	params_history: None,
-# 	params_dataPlace : None,
-# }
-
-# def parseParams(args):
-# 	#--paramA = valorA --paramB = valorB ...
-# 	argString = reduce(lambda a, b: a+' '+b, args)
-# 	paramDict = {}
-# 	for paramKey in params.iterkeys():
-# 		regex = re.compile('(?:.+?|^)--'+paramKey+'\s*=\s*(?P<'+paramKey+'>.+?)(?:\s|$)') #odio las expresiones regulares profundamente
-# 		valor = regex.match(argString)
-# 		if (valor):
-# 			paramDict.update(valor.groupdict())
-# 	for k, v in paramDict.iteritems():
-# 		if k == params_readTime or k == params_history:
-# 			paramDict[k] = int(v)
-# 		elif k == params_dataPlace:
-# 			paramDict[k] = map(int, v.split(','))
-# 	return paramDict
 
 def entropia(counter):
 	#counter[s] = cantidad de apariciones del símbolo s (positivas)
@@ -92,20 +56,6 @@ def calcularInfoPorHost(dump):
 	todosLosPedidos = sum(todosLosHosts.itervalues())
 	infoPorHost = {}
 	for d in todosLosHosts.iteritems():
+		#para cada host obtenido, calculamos la cantidad de información que aporta al aparecer.	
 		infoPorHost[d[0]] = -math.log(float(d[1])/todosLosPedidos)
 	return infoPorHost
-
-
-# if __name__ == '__main__':
-# 	cantidad = collections.Counter()
-# 	paramDict = parseParams(sys.argv[1:])
-# 	if paramDict[params_timePlace] == None or paramDict[params_readTime] == None or paramDict[params_history] == None:
-# 		l = sys.stdin.readline()
-# 		while l:
-# 			data = l.split()
-# 			mensaje = reduce(lambda a, b: a+'-'+b, [data[i] for i in paramDict[params_dataPlace]])
-# 			cantidad[mensaje] += 1
-# 			l = sys.stdin.readline()
-# 		print entropia(cantidad)
-# 		print 'ahora sabés más'
-# 		exit()
