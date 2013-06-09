6b10e7e9c23ca1fbbfa6de45642841b90b5a964f
ChurroCode <code@churro.com>
2013-06-09 04:52:21 -0300

    agregue que se puedan hacer dumps del rtt promedio del enlace

--- TP2/src/mediciones.sh
+++ TP2/src/mediciones.sh
@@ -5,27 +5,30 @@ mask_src=$2
 host_dst=$3
 mask_dst=$4
 host_final=$5
+dump_file=$6
 
-if [ -z "$host_dst" -o -z "$mask_dst" -o -z "$host_src" -o -z "$mask_src" -o -z "$host_final" ]
+if [ -z "$host_dst" -o -z "$mask_dst" -o -z "$host_src" -o -z "$mask_src" -o -z "$host_final" -o -z "$dump_file" ]
 then
-	echo "Usar ./mediciones.sh <host_src> <mask_src> <host_dst> <mask_dst> <host_final>"
+	echo "Usar ./mediciones.sh <host_src> <mask_src> <host_dst> <mask_dst> <host_final> <dump_file>"
 	exit 0
 fi
 
 tr_file=traceroute_dump.txt
 
 traceroute $host_final > $tr_file
-hops=$(python parser.py $host_src $mask_src $host_dst $mask_dst)
+hops=$(python rangoDeIPs.py $host_src $mask_src $host_dst $mask_dst)
 
 first=`echo $hops | awk '{print $1}'`
 max=`echo $hops | awk '{print $2}'`
+
+touch $dump_file
 
 for i in $(seq 1 100)
 do
 	echo TR $i Hop inicial: $first  Hop final: $max
 
-	traceroute -f $first -m $max $host_final > $tr_file
-	hops=$(python parser.py $host_src $mask_src $host_dst $mask_dst)
+	traceroute -f $first -m $max -q 5 $host_final > $tr_file
+	hops=$(python rangoDeIPs.py $host_src $mask_src $host_dst $mask_dst $tr_file)
 	
 	#si el python devuelve un -1, bash lo ve como un 255 (alta saturacion xD)
 	if [ $? = 255 ]; then
@@ -35,6 +38,9 @@ do
 
 	first=`echo $hops | awk '{print $1}'`
 	max=`echo $hops | awk '{print $2}'`
+	echo $hops | awk '{print $3}' >> $dump_file
+
+
 done
 
 rm -f $tr_file
