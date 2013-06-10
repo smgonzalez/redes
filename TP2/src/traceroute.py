c74e5bf6bdb7e4670e2f78bc14ae59fc2b43808c
victoria <vabrevaya@ubuntu.(none)>
2013-06-10 01:55:55 -0300

    Cambios en Traceroute

--- TP2/src/traceroute.py
+++ TP2/src/traceroute.py
@@ -2,43 +2,87 @@
 
 from scapy.all import *
 from optparse import OptionParser
+import time
+import datetime
 
-def traceroute(host, ttl_max=64):
-	route=[]
+def traceroute(host, ttl_max=64, pkgs_per_ttl=1):
+	
+	ttl = int(ttl_max)
+	num_pkgs = int(pkgs_per_ttl)
+	
+	# num_pkgs: Cantidad de paquetes que se envian por cada ttl
+	#route=[]
 	pkg=IP(dst=host) / ICMP() / 'Pingeando host'
-
-#	try:
-	for i in range(0, ttl_max+1):
-		rep = None
-		print 'pingeando i:', i
+	
+	print datetime.datetime.now()
+	
+	for i in range(1, ttl+1):
+		
+		#print 'pingeando i:', i
 		pkg.ttl = i
-		rep = sr1(pkg, verbose=0, timeout=3)
-
-		if rep == None:
-			ip = '-'
-		else:
-			ip = rep[0].src
+		strRes = 'Hop ' + str(i) + '\t'
+		
+		ready = 0
+		contin = 0
+		ip_ok = 0
+		
+		rtts = [0] * num_pkgs	
+		
+		for p in range(1, num_pkgs+1):
 			
-		route.append(ip)
+			ans = None
+			
+			t0 = time.time()
+			ans = sr1(pkg, verbose=0, timeout=3)
+			rtt = time.time() - t0
+			
+			if ans == None:
+				ip = '-'
+				# Asumo que el resto de los intentos tambien van a fallar
+				#contin = 1
+				#break
+			else:
+				ip = ans[0].src
+				ip_ok = 1
+				
+				# Solo entran al promedio los rtts de respuestas correctas
+				rtts[p-1] = rtt
+				
+				if ans[1].type == 0:		# Recibi paquete ICMP del tipo echo-reply
+					ready = 1
+					break
+				
+			strRes += ip + '\t' + str(round(rtt,4)*100) + 'ms\t'
 
-		if ip == host:
+			if ip == host:
+				ready = 1
+				break
+
+		if ready:
 			break
+		elif contin:
+			continue
+			
+		#route.append(ip)
+		# Saco RTT promedio
+		if ip_ok:
+			avgRTT = reduce(lambda x,y:float(x)+float(y), rtts)/len(rtts)
+			strRes += 'RTT Promedio: ' + str(round(avgRTT, 4)*100) + 'ms'
+			print strRes
 
-#	except:
-#		return -1
-
-	i=1
-	print 'Ruta:'
-	for ip in route:
-		print i, ')', ip
-		i+=1
+#	i=1
+#	print 'Ruta:'
+#	for ip in route:
+#		print i, ')', ip
+#		i+=1
 	
 	return 0
 
 
 def definirParamertos(parser):
-	parser.add_option("-t", "--time-to-live-max", default=64, dest='ttl', help='tiempo de vida maximo de los paquetes ICMP (default = 64)')
-	parser.add_option("-o", "--host", default='localhost', dest='host', help='ip destino del traceroot (default = localhost)')
+	parser.add_option("-t", "--time-to-live-max", default=64, dest='ttl', help='Tiempo de vida maximo de los paquetes ICMP (default = 64)')
+	parser.add_option("-o", "--host", default='localhost', dest='host', help='IP destino del traceroot (default = localhost)')
+	parser.add_option("-p", "--packages-per-ttl", default=1, dest='pkgs_per_ttl', help='Cantidad de paquetes a enviar por cada ttl (default = 1)')
 	
 	(params, args)=parser.parse_args()
 
@@ -47,7 +91,7 @@ def definirParamertos(parser):
 
 if __name__ == '__main__':
 
-	usage = "usage: sudo %prog [options] -o <host>"
+	usage = "usage: sudo ./traceroute.py [options] -o <host>"
 
 	#seteamos parser de los parametros
 	parser=OptionParser(usage=usage)
@@ -58,7 +102,7 @@ if __name__ == '__main__':
 		parser.print_help()
 		exit()
 
-	exit_code = traceroute(params.host, params.ttl)
+	exit_code = traceroute(params.host, params.ttl, params.pkgs_per_ttl)
 
 	if exit_code == -1:
 		print 'Hubo un problema al generar la ruta'
